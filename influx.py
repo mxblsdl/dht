f1f323b90e477fc30771ba0d66b4e4adadeef3ca
mxblsdl <maxblasdel@gmail.com>
2021-09-25 20:57:57 -0700

    remove test connection file

--- influx.py
+++
@@ -1,6 +0,0 @@
-from influxdb import InfluxDBClient
-
-client = InfluxDBClient(host = '167.71.158.245', port = 8086, username = 'shiny', password = 'metrics', database = "temperature")
-
-print(client.query("SHOW MEASUREMENTS"))
-
