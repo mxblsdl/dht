60baabaca948ba336b9d54edb4d32cf64c4d6b7d
mxblsdl <maxblasdel@gmail.com>
2021-10-08 04:09:58 +0100

    add sys sleep

--- temp.py
+++ temp.py
@@ -1,5 +1,6 @@
 import Adafruit_DHT as adht 
-import logging 
+import logging
+import time
 
 '''
 Logs temperature and humidity to a log file every 30 seconds. 
@@ -7,5 +8,6 @@ Logs temperature and humidity to a log file every 30 seconds.
 logging.basicConfig(filename='temperature.log', filemode='a', format='%(created)1s %(message)s', level=logging.INFO) 
 
 while True:     
-    h,t = adht.read_retry(adht.DHT22, 4, delay_seconds=30)
+    h,t = adht.read_retry(adht.DHT22, 4)
     logging.info(f'Temp={round(t, 1)} C and Humidity={round(h, 1)}')
+    time.sleep(30)
