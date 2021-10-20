79d39484bac75777cb586152aa7dd502b9b3601d
mxblsdl <maxblasdel@gmail.com>
2021-10-19 20:04:33 -0700

    update docs

--- truncate.sh
+++ truncate.sh
@@ -1,3 +1,4 @@
 # used to periodically truncate the log file being created
-
+# Writes the file with 0 bytes
+# This does not interfere with continuous writing values to the file
 truncate -s 0 temperature.log
